93c128e617d2921ef80fd802c1d748602a59cd60
SamCherry09 <samcherry2006@outlook.de>
2022-06-10 12:47:04 +0200

    Elements and Structure (Picture not loading)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
